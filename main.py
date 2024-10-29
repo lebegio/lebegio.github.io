375c8e34655a1c282185015f276da7acc4c3accf
Aliaksandr Mandryk <lebegio@aliaksandrs-macbook-air-2.home>
2024-10-29 02:03:59 +0100

    test commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import local_config as config
 # Replace with your bot token
 #web_app_url = "http://127.0.0.1:5000/"  # Replace with your server URL
 web_app_url = "https://www.google.com/"
+# test commit after repo renaming
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # Create a button with WebApp info to open the inline web app
