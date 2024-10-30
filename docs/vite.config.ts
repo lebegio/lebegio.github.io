67418fd484d3b31e7cdff9265e536f24dd22f3c2
Aliaksandr Mandryk <lebegio@aliaksandrs-macbook-air-2.home>
2024-10-30 02:27:23 +0100

    added docs in base

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -5,5 +5,5 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: '/lebegio.github.io/',
+  base: '/lebegio.github.io/docs/',
 });
