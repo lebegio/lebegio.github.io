b21acad399961950c6b5879dbbf924e5863c48af
Aliaksandr Mandryk <lebegio@aliaksandrs-macbook-air-2.home>
2024-10-31 23:28:16 +0100

    test

--- web_client/src/main.tsx
+++ web_client/src/main.tsx
@@ -3,7 +3,7 @@ import App from './App';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-// this manifest is used temporarily for development purposes
+// this manifest is used temporarily for development purposes 
 const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
