69a5555516f41e3d0cda552d932817aa9b435160
Aliaksandr Mandryk <lebegio@aliaksandrs-macbook-air-1.home>
2024-11-07 05:42:07 +0100

    upd v1

---
+++ func/private_config.ts
@@ -0,0 +1,12 @@
+// mnemonic of the wallet required for deploy
+export const MNEMONIC_V0 = "flat aunt rely father skate rookie clean margin beyond worth shrug aware sorry duck rate ahead then year police embody insane reveal height fog";
+
+// address of the deployed contract
+export const CONTRACT_ADDRESS_V0 = "EQBEnDLl2jh8qXH5dg0b-0xl51LADemfnM1Q1t_rRxBJWxev"
+
+// mnemonic of the wallet required for deploy
+export const MNEMONIC = "flight artwork error major tortoise kingdom bring ghost fish fish shoe candy net empower lumber fish aware embody rubber canal sell shoot decide bamboo";
+
+// address of the deployed contract
+export const CONTRACT_ADDRESS = "EQBLVpfHyZjFs6J9hlEbrdYkttqI4gaRnB_4mz6_1L5fqTJI"
+    
